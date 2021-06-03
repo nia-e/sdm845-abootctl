f183bb8241641a69a50fb1741ba2434f61e1fbeb
ai-b-z <pixelpower.ro@gmail.com>
2021-06-03 22:08:24 +0300

    initialise repo, only reading works so far

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+extern crate clap;
+use gpt;
+use clap::{Arg, App};
+use std::path::Path;
+use std::iter::FromIterator;
+
+fn main() {
+
+    // CLI stuff
+    let matches = App::new("bootctl")
+        .version("0.1")
+        .author("Aissa Z. B. <aissa.zenaida@pm.me>")
+        .about("Switch active bootloader slot on SDM845 OnePlus devices")
+        .arg(Arg::with_name("SLOT")
+            .help("Slot to set as active (0 or 1)")
+            .required(true)
+            .index(1))
+        .get_matches();
+    
+    let slot = matches.value_of("SLOT").unwrap().parse::<i32>().unwrap();
+    
+    set_slot(&slot);
+    // println!("Invalid SLOT number; see bootctl --help");
+}
+
+fn set_slot(slot: &i32) {
+    
+    let disk_path = Path::new("/dev/sde");
+    let size =  gpt::disk::DEFAULT_SECTOR_SIZE;
+
+    let header = gpt::header::read_header(disk_path, size).unwrap();
+    let partitions_btm = gpt::partition::read_partitions(disk_path, &header, size).unwrap();
+    //Change btreemap to vector
+    let mut partitions = Vec::from_iter(partitions_btm);
+    //Find relevant partitions
+    let boot_a = &partitions[10].1;
+    let boot_b = &partitions[38].1;
+
+    println!("boot_a: {}; boot_b: {}", boot_a.flags, boot_b.flags);
+}
