5b0168221defc241b4a656c989f77f133a292c04
ai-b-z <pixelpower.ro@gmail.com>
2021-06-04 17:16:52 +0300

    updated autorship

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Utility to control boot slots on OnePlus SDM845 devices from mainline Linux by f
 
 WARNING: THIS MAY BRICK YOUR DEVICE, WIPE YOUR PARTITION TABLE, OR AWAKEN THE GREAT OLD ONES OF R'LYEH. NO MATTER WHAT THIS UTILITY CAUSES, MATERIAL OR ESOTERIC, YOU HAVE BEEN WARNED.
 
-Written by Aissa Z. B. with great help from Caleb C.
+Written by Aissa Z. B. <aissa.zenaida@pm.me> with great help from Caleb C. <caleb@connolly.tech>
--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn main() {
     // CLI stuff
     let matches = App::new("abootctl")
         .version("0.2.0")
-        .author("Caleb C., Aissa Z. B. <aissa.zenaida@pm.me>")
+        .author("Caleb C. <caleb@connolly.tech>, Aissa Z. B. <aissa.zenaida@pm.me>")
         .about("Switch active bootloader slot on SDM845 OnePlus devices. THIS MAY BRICK YOUR DEVICE - USE WITH CARE")
         .arg(Arg::with_name("SLOT")
             .help("Slot to set as active (0 or 1)")
