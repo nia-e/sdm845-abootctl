6795ca1e39c8477fdbe5c7de65ad149f72d7aaaf
ai-b-z <pixelpower.ro@gmail.com>
2021-06-04 10:52:04 +0300

    prevented possible headache from mixing tabs and spaces

--- src/main.rs
+++ src/main.rs
@@ -33,53 +33,53 @@ fn set_slot(slot: &i32) {
     //Change btreemap to vector
     let partitions = Vec::from_iter(partitions_btm);
     //Find relevant partitions
-	//This is probably partly redundant but it works
-	unsafe {
+    //This is probably partly redundant but it works
+    unsafe {
 
         let mut boot_y: u64 = 0;
         let mut boot_n: u64 = 0;
 
-		if *slot as i32 == 0 {
+        if *slot as i32 == 0 {
 
-    		boot_y = *&partitions[10].1.flags;
-    		boot_n = *&partitions[38].1.flags;
-			//Flags are read first even though they should never differ from baseline, just in case
-			boot_y = enable_aboot(boot_y);
-			boot_n = disable_aboot(boot_n);
-			//Prepare changes to rewrite
-			let a_flags = gpt::partition::PartitionAttributes::from_bits_unchecked(boot_y);
-			let b_flags = gpt::partition::PartitionAttributes::from_bits_unchecked(boot_n);
-		}
-		else if *slot as i32 == 1 {
+            boot_y = *&partitions[10].1.flags;
+            boot_n = *&partitions[38].1.flags;
+            //Flags are read first even though they should never differ from baseline, just in case
+            boot_y = enable_aboot(boot_y);
+            boot_n = disable_aboot(boot_n);
+            //Prepare changes to rewrite
+            let a_flags = gpt::partition::PartitionAttributes::from_bits_unchecked(boot_y);
+            let b_flags = gpt::partition::PartitionAttributes::from_bits_unchecked(boot_n);
+        }
+        else if *slot as i32 == 1 {
 
-			//Same as above
-			boot_y = *&partitions[38].1.flags;
-    		boot_n = *&partitions[10].1.flags;
-			boot_y = enable_aboot(boot_y);
-			boot_n = disable_aboot(boot_n);
-			let a_flags = gpt::partition::PartitionAttributes::from_bits_unchecked(boot_n);
-			let b_flags = gpt::partition::PartitionAttributes::from_bits_unchecked(boot_y);
-		}
+            //Same as above
+            boot_y = *&partitions[38].1.flags;
+            boot_n = *&partitions[10].1.flags;
+            boot_y = enable_aboot(boot_y);
+            boot_n = disable_aboot(boot_n);
+            let a_flags = gpt::partition::PartitionAttributes::from_bits_unchecked(boot_n);
+            let b_flags = gpt::partition::PartitionAttributes::from_bits_unchecked(boot_y);
+        }
 
-	    //Rewrite changes to GPT table
-    	println!("active slot: {}; inactive slot: {}", boot_y, boot_n);
-	}
+        //Rewrite changes to GPT table
+        println!("active slot: {}; inactive slot: {}", boot_y, boot_n);
+    }
 }
 
 fn enable_aboot(bootflags: u64) -> u64 {
 
-	//Sets 5th bit to 1, sets active boot partition
-	let mut t_bootflags = bootflags;
-	t_bootflags |= 0b0000_1000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000;
-	//KEEPING THIS OFF, MAY BRICK IF ENABLED
-	//bootflags &= 0b1111_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000;
-	return t_bootflags;
+    //Sets 5th bit to 1, sets active boot partition
+    let mut t_bootflags = bootflags;
+    t_bootflags |= 0b0000_1000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000;
+    //KEEPING THIS OFF, MAY BRICK IF ENABLED
+    //bootflags &= 0b1111_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000;
+    return t_bootflags;
 }
 
 fn disable_aboot(bootflags: u64) -> u64 {
 
-	//Sets 5th bit to 0, unsets active boot partition
-	let mut t_bootflags = bootflags;
-	t_bootflags &= 0b0000_1000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000;
-	return t_bootflags;
+    //Sets 5th bit to 0, unsets active boot partition
+    let mut t_bootflags = bootflags;
+    t_bootflags &= 0b0000_1000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000;
+    return t_bootflags;
 }
