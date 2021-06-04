c55e0adcf762f3fa992b6282d409977bd0e9dc19
ai-b-z <pixelpower.ro@gmail.com>
2021-06-04 14:43:57 +0300

    code cleanup

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ use clap::{Arg, App};
 use std::path::Path;
 use std::process;
 
+const BOOT_A_PARTNUM: u32 = 11;
+const BOOT_B_PARTNUM: u32 = 39;
+const DEVICE_PATH: &str = "/dev/sde";
+
 fn main() {
 
     // CLI stuff
@@ -20,74 +24,65 @@ fn main() {
     let slot = matches.value_of("SLOT").unwrap().parse::<i32>().unwrap();
 
     set_slot(&slot);
-    // println!("Invalid SLOT number; see bootctl --help");
 }
 
 fn set_slot(slot: &i32) {
 
     //Open relevant GPT stuff
-    let disk_path = Path::new("/dev/sde");
+    let disk_path = Path::new(DEVICE_PATH);
     let size = gpt::disk::LogicalBlockSize::Lb4096;
 
     let header = gpt::header::read_header(disk_path, size).unwrap();
     let mut partitions = gpt::partition::read_partitions(disk_path, &header, size).unwrap();
     let mut disk = gpt::disk::read_disk(&disk_path).unwrap();
-    //println!("{}", partitions[&10]);
 
     //Find relevant partitions
     //Flags are read first even though they should never differ from baseline, just in case
-    let mut boot_a_flags = *&partitions[&11].flags;
-    let mut boot_b_flags = *&partitions[&39].flags;
-    let mut success: bool = false;
+    let mut boot_a_flags = *&partitions[&BOOT_A_PARTNUM].flags;
+    let mut boot_b_flags = *&partitions[&BOOT_B_PARTNUM].flags;
 
     if *slot as i32 == 0 {
 
+        //Change _a and _b boot partition flags
         boot_a_flags = enable_aboot(boot_a_flags);
         boot_b_flags = disable_aboot(boot_b_flags);
-        success = true;
     }
     else if *slot as i32 == 1 {
 
         //Same as above
         boot_b_flags = enable_aboot(boot_b_flags);
         boot_a_flags = disable_aboot(boot_a_flags);
-        success = true;
     }
-    //else { a_flags = false; b_flags = false; }
+    else { eprintln!("Error: could not read partition table headers or invalid slot number specified"); process::exit(1); }
 
     //Check flags have been updated; sanity
     //Rewrite changes to GPT table
-    if success {
 
-        let mut new_boot_a = partitions[&11].clone();
-        let mut new_boot_b = partitions[&39].clone();
+    let mut new_boot_a = partitions[&BOOT_A_PARTNUM].clone();
+    let mut new_boot_b = partitions[&BOOT_B_PARTNUM].clone();
 
-        new_boot_a.flags = boot_a_flags;
-        new_boot_b.flags = boot_b_flags;
-        partitions.insert(11, new_boot_a);
-        partitions.insert(39, new_boot_b);
-        disk.update_partitions(partitions).unwrap();
-        disk.write().unwrap();
-    }
-
-    else { eprintln!("Error: could not read partition table headers or invalid slot number specified"); process::exit(1); }
-    //println!("active slot: {}; inactive slot: {}", boot_y, boot_n);
+    new_boot_a.flags = boot_a_flags;
+    new_boot_b.flags = boot_b_flags;
+    partitions.insert(BOOT_A_PARTNUM, new_boot_a);
+    partitions.insert(BOOT_B_PARTNUM, new_boot_b);
+    disk.update_partitions(partitions).unwrap();
+    disk.write().unwrap();
 }
 
 fn enable_aboot(bootflags: u64) -> u64 {
 
     //Sets 5th bit to 1, sets active boot partition
-    let mut t_bootflags = bootflags;
-    t_bootflags |= 0b0000_1000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000;
-    //KEEPING THIS OFF, MAY BRICK IF ENABLED
+    let mut tmp_bootflags = bootflags;
+    tmp_bootflags |= 0b0000_1000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000;
+    //KEEPING THIS OFF FOR NOW, MAY BRICK IF ENABLED
     //bootflags &= 0b1111_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000;
-    return t_bootflags;
+    return tmp_bootflags;
 }
 
 fn disable_aboot(bootflags: u64) -> u64 {
 
     //Sets 5th bit to 0, unsets active boot partition
-    let mut t_bootflags = bootflags;
-    t_bootflags &= 0b0000_1000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000;
-    return t_bootflags;
+    let mut tmp_bootflags = bootflags;
+    tmp_bootflags &= 0b0000_1000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000;
+    return tmp_bootflags;
 }
