0cb1be23f51732dca38eefef9956a663701ad3b4
ai-b-z <pixelpower.ro@gmail.com>
2021-06-05 17:51:56 +0300

    rustfmt and clippy suggestions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{Arg, App};
+use clap::{App, Arg};
 use modular_bitfield::prelude::*;
 use std::convert::TryInto;
 
@@ -7,16 +7,17 @@ mod partitions;
 #[bitfield(bits = 8)]
 #[derive(Debug)]
 struct SlotInfo {
-    #[skip] __: B2,
+    #[skip]
+    __: B2,
     is_active: B1,
-    #[skip] __: B3,
+    #[skip]
+    __: B3,
     boot_successful: B1,
     is_unbootable: B1,
 }
 
 fn main() {
     // CLI stuff
-    //Ideas: -r read-only (default), --set or -s set to SLOT, --debug dump all flags
     let matches = App::new("abootctl")
         .version("0.5.0")
         .author("Caleb C. <caleb@connolly.tech>, Aissa Z. B. <aissa.zenaida@pm.me>")
@@ -48,30 +49,43 @@ fn main() {
     let (flags_a, flags_b, slot_a, slot_b) = get_slot_info(debug);
 
     mode = matches.value_of("mode").unwrap_or("r");
-    slot = matches.value_of("slot").unwrap().parse::<i32>().unwrap_or(-1);
+    slot = matches
+        .value_of("slot")
+        .unwrap()
+        .parse::<i32>()
+        .unwrap_or(-1);
 
     //Checking CLI args
-    if !(mode.eq("r") || mode.eq("w")) { panic!("ERROR: Invalid mode specified"); }
-    if !(slot == 0 || slot == 1) { panic!("ERROR: Invalid slot specified"); }
+    if !(mode.eq("r") || mode.eq("w")) {
+        panic!("ERROR: Invalid mode specified");
+    }
+    if !(slot == 0 || slot == 1) {
+        panic!("ERROR: Invalid slot specified");
+    }
 
     if mode.eq("r") {
         println!("Slot A info: {:?}", slot_a);
         println!("Slot B info: {:?}", slot_b);
+    } else {
+        set_slot(&slot, flags_a, flags_b);
     }
-
-    else { set_slot(&slot, flags_a, flags_b); }
 }
 
 fn get_slot_info(debug: bool) -> (u64, u64, SlotInfo, SlotInfo) {
     let (boot_a, boot_b, _) = partitions::get_boot_partitions();
-    if debug { println!("boot_a: {:#018b} boot_b: {:#018b}", boot_a.flags >> 48, boot_b.flags >> 48); }
-    let slot_a = SlotInfo::from_bytes([(((boot_a.flags >> 48) & 0xFF)).try_into().unwrap()]);
-    let slot_b = SlotInfo::from_bytes([(((boot_b.flags >> 48) & 0xFF)).try_into().unwrap()]);
-    return (boot_a.flags >> 48, boot_b.flags >> 48, slot_a, slot_b)
+    if debug {
+        println!(
+            "boot_a: {:#018b} boot_b: {:#018b}",
+            boot_a.flags >> 48,
+            boot_b.flags >> 48
+        );
+    }
+    let slot_a = SlotInfo::from_bytes([((boot_a.flags >> 48) & 0xFF).try_into().unwrap()]);
+    let slot_b = SlotInfo::from_bytes([((boot_b.flags >> 48) & 0xFF).try_into().unwrap()]);
+    return (boot_a.flags >> 48, boot_b.flags >> 48, slot_a, slot_b);
 }
 
 fn set_slot(slot: &i32, flags_a: u64, flags_b: u64) {
-
     let new_flags_a;
     let new_flags_b;
 
@@ -79,13 +93,13 @@ fn set_slot(slot: &i32, flags_a: u64, flags_b: u64) {
         //Change _a and _b boot partition flags
         new_flags_a = enable_aboot(flags_a);
         new_flags_b = disable_aboot(flags_b);
-    }
-    else if *slot as i32 == 1 {
+    } else if *slot as i32 == 1 {
         //Same as above
         new_flags_b = enable_aboot(flags_b);
         new_flags_a = disable_aboot(flags_a);
+    } else {
+        panic!("Error: could not read partition table headers or invalid slot number specified");
     }
-    else { panic!("Error: could not read partition table headers or invalid slot number specified"); }
 
     //Get actual boot partition objects
     let (mut boot_a, mut boot_b, path) = partitions::get_boot_partitions();
@@ -95,19 +109,19 @@ fn set_slot(slot: &i32, flags_a: u64, flags_b: u64) {
 }
 
 fn enable_aboot(bootflags: u64) -> u64 {
-
     //Sets 5th bit to 1, sets active boot partition
     let mut tmp_bootflags = bootflags;
-    tmp_bootflags |= 0b0000_1000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000;
+    tmp_bootflags |=
+        0b0000_1000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000;
     //KEEPING THIS OFF FOR NOW, MAY BRICK IF ENABLED
     //bootflags &= 0b1111_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000;
     return tmp_bootflags;
 }
 
 fn disable_aboot(bootflags: u64) -> u64 {
-
     //Sets 5th bit to 0, unsets active boot partition
     let mut tmp_bootflags = bootflags;
-    tmp_bootflags &= 0b1111_0111_1111_1111_1111_1111_1111_1111_1111_1111_1111_1111_1111_1111_1111_1111;
+    tmp_bootflags &=
+        0b1111_0111_1111_1111_1111_1111_1111_1111_1111_1111_1111_1111_1111_1111_1111_1111;
     return tmp_bootflags;
 }
--- src/partitions.rs
+++ src/partitions.rs
@@ -1,76 +1,91 @@
-use std::path::Path;
-use gpt::partition::{Partition};
 use gpt::disk;
+use gpt::partition::Partition;
 use std::collections::BTreeMap;
 use std::io;
+use std::path::Path;
 
 fn get_partitions_for_dev(dev_path: &Path) -> Result<BTreeMap<u32, Partition>, io::Error> {
-
-	println!("Testing path {}", dev_path.display());
-	let size = disk::LogicalBlockSize::Lb4096;
-	let header = gpt::header::read_header(dev_path, size);
-	match header {
-		Ok(header) => gpt::partition::read_partitions(dev_path, &header, size),
-		Err(e) => Err(e),
-	}
+    println!("Testing path {}", dev_path.display());
+    let size = disk::LogicalBlockSize::Lb4096;
+    let header = gpt::header::read_header(dev_path, size);
+    match header {
+        Ok(header) => gpt::partition::read_partitions(dev_path, &header, size),
+        Err(e) => panic!("{}", e),
+    }
 }
 
 pub fn get_boot_partitions() -> (Partition, Partition, String) {
-	//Open relevant GPT stuff
+    //Open relevant GPT stuff
 
-	let blockdevs = block_utils::get_block_devices();
-	assert!(!blockdevs.is_err());
+    let blockdevs = block_utils::get_block_devices();
+    assert!(!blockdevs.is_err());
 
-	let blockdevs = blockdevs.unwrap();
+    let blockdevs = blockdevs.unwrap();
 
-	let dev_path = blockdevs.into_iter()
-		.find(|dev_path| { match get_partitions_for_dev(dev_path.as_path()) {
-			Ok(dev) => dev.values().find(|p| p.name == "boot_a").is_some(),
-			Err(e) => false,
-		}});
-	if dev_path.is_none() {
-		panic!("Failed to find boot_a, does this device have slots?");
-	}
-	let dev_path = dev_path.unwrap();
+    let dev_path =
+        blockdevs.into_iter().find(
+            |dev_path| match get_partitions_for_dev(dev_path.as_path()) {
+                Ok(dev) => dev.values().any(|p| p.name == "boot_a"),
+                Err(_e) => false,
+            },
+        );
+    if dev_path.is_none() {
+        panic!("Failed to find boot_a, does this device have slots?");
+    }
+    let dev_path = dev_path.unwrap();
 
-	let all_partitions =  get_partitions_for_dev(dev_path.as_path()).unwrap();
-	let boot_a = all_partitions.values().find(|x| x.name == "boot_a").unwrap().clone();
-	let boot_b = all_partitions.values().find(|x| x.name == "boot_b").unwrap().clone();
+    let all_partitions = get_partitions_for_dev(dev_path.as_path()).unwrap();
+    let boot_a = all_partitions
+        .values()
+        .find(|x| x.name == "boot_a")
+        .unwrap()
+        .clone();
+    let boot_b = all_partitions
+        .values()
+        .find(|x| x.name == "boot_b")
+        .unwrap()
+        .clone();
 
-	// for dev_path in blockdevs {
-	// 	let header = gpt::header::read_header(dev_path.as_path(), size).unwrap();
-	// 	let mut partitions = gpt::partition::read_partitions(dev_path.as_path(), &header, size).unwrap();
-	// 	let res = partitions.values().try_find(|x| x.name == "boot_a");
-	// 	if res.is_err() {
-	// 		continue;
-	// 	}
-	// }
+    // for dev_path in blockdevs {
+    // 	let header = gpt::header::read_header(dev_path.as_path(), size).unwrap();
+    // 	let mut partitions = gpt::partition::read_partitions(dev_path.as_path(), &header, size).unwrap();
+    // 	let res = partitions.values().try_find(|x| x.name == "boot_a");
+    // 	if res.is_err() {
+    // 		continue;
+    // 	}
+    // }
 
-	// let boot_a = partitions.values().find(|x| x.name == "boot_a").unwrap();
+    // let boot_a = partitions.values().find(|x| x.name == "boot_a").unwrap();
 
-	return (boot_a, boot_b, dev_path.as_path().to_str().unwrap().to_string());
+    return (
+        boot_a,
+        boot_b,
+        dev_path.as_path().to_str().unwrap().to_string(),
+    );
 }
 
 pub fn set_boot_partitions(boot_a: Partition, boot_b: Partition, path: String) {
-
     //Opens relevant stuff
     let path = Path::new(&path);
     let config = gpt::GptConfig::new();
     let config = config.writable(true); //config needs to be shadowed here for some reason
-    let mut disk = config.open(&path).unwrap(); //Should be fine since for this function to run get_boot_partitions() must have succeeded
+    let mut disk = config.open(path).unwrap(); //Should be fine since for this function to run get_boot_partitions() must have succeeded
     let part_table = disk.partitions();
     let mut new_part_table = part_table.clone();
 
     for (key, part) in part_table.iter() {
         if part.name == "boot_a" {
             new_part_table.insert(*key, boot_a.clone());
-        }
-        else if part.name == "boot_b" {
+        } else if part.name == "boot_b" {
             new_part_table.insert(*key, boot_b.clone());
         }
     }
 
-    disk.update_partitions(new_part_table);
+    let _part_res = disk.update_partitions(new_part_table);
+    match _part_res {
+        Ok(_part_res) => println!("Updated partition table"),
+        Err(e) => panic!("{}", e),
+    }
 
     let _final_res = disk.write_inplace();
     match _final_res {
